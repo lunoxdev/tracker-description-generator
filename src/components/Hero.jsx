1391179735159851c41a54f8f42e8e2de985a702
lunoxdev <lunox.code@gmail.com>
2025-02-21 19:59:25 -0600

    update opacity

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -182,7 +182,7 @@ const Hero = () => {
           {/* COPY BUTTON */}
           <button 
             onClick={copyToClipboard} 
-            className="w-full max-w-[400px] p-3 rounded-xl text-center flex items-center justify-between bg-black border-2 border-[#21d2fe] transition-all duration-300 hover:opacity-80 hover:ring-2 hover:ring-[#21d2fe] hover:ring-offset-2"
+            className="w-full max-w-[400px] p-3 rounded-xl text-center flex items-center justify-between bg-black border-2 border-[#21d2fe] transition-all duration-300 hover:opacity-90 hover:ring-2 hover:ring-[#21d2fe] hover:ring-offset-2"
           >
             {/* Texto centrado */}
             <span className="flex-1 text-center truncate">
